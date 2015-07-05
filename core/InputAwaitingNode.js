418ea7693696fa2dcd8969d8ceeb6767ce3650a0
rangermauve <RangerMauve@hotmail.com>
2015-07-05 14:20:26 -0400

    Add initial state arguemtn to InputAwaitingNode

--- core/InputAwaitingNode.js
+++ core/InputAwaitingNode.js
@@ -1,4 +1,5 @@
 var DeclarativeNode = require("./DeclarativeNode");
+var xtend = require("xtend");
 
 module.exports = InputAwaitingNode;
 
@@ -7,14 +8,15 @@ module.exports = InputAwaitingNode;
  * have been recieved at least once, and every time one updates after that.
  * Takes a list of outputs that can be sent to via `this.output.OUTPUTNAME(data)`
  * Persistant data should be saved in properties of `this`
- * @param  {String}   name    The name of the new Node type being created
- * @param  {Array}    inputs  List of input names
- * @param  {Array}    outputs List of output names this node will provide
- * @param  {Function} dispose Optional function that will be called to clean up the state of the node
- * @param  {Function} update  Function that gets called when inputs update. Takes arguments that correspond to the list of inputs
- * @return {Node}             The new Node definition
+ * @param  {String}   name          The name of the new Node type being created
+ * @param  {Array}    inputs        List of input names
+ * @param  {Array}    outputs       List of output names this node will provide
+ * @param  {Function} update        Function that gets called when inputs update. Takes arguments that correspond to the list of inputs
+ * @param  {Function} initial_state Optional funcction that returns the initial state for the
+ * @param  {Function} dispose       Optional function that will be called to clean up the state of the node
+ * @return {Node}                   The new Node definition
  */
-function InputAwaitingNode(name, inputs, outputs, update, dispose) {
+function InputAwaitingNode(name, inputs, outputs, update, initial_state, dispose) {
 	var input_map = inputs.reduce(function (map, name) {
 		map[name] = function (data) {
 			this.input[name] = data;
@@ -29,11 +31,10 @@ function InputAwaitingNode(name, inputs, outputs, update, dispose) {
 
 	return DeclarativeNode(name, input_map, outputs, initial_state, dispose);
 
-	function initial_state() {
-		return {
-			input: {},
-			_input: {}
-		};
+	function state() {
+		var state = default_initial_state();
+		if (initial_state)
+			stae = xtend(state, initial_state());
 	}
 
 	function as_list(input) {
@@ -47,4 +48,11 @@ function InputAwaitingNode(name, inputs, outputs, update, dispose) {
 			return input[name];
 		});
 	}
+}
+
+function default_initial_state() {
+	return {
+		input: {},
+		_input: {}
+	};
 }
